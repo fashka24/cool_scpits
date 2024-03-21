97e49a2a6104b88d10c8eced23e40350fe94115c
fashka24 <160354861+fashka24@users.noreply.github.com>
2024-03-21 22:22:57 +0500

    add pole game

---
+++ sripts/mygames/pole.cpp
@@ -0,0 +1,48 @@
+/*
+    author: ktfk
+    to run:
+        clang++ POLE.CPP -o EXES/POLE.EXE
+        OR
+        clang++ POLE.CPP -o EXES/POLE.O
+        ./EXES/POLE
+
+
+*/
+
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+bool pole[] = {
+    0,0,0,
+    0,0,0,
+    0,0,0,
+};
+int main() {
+    int cur = 0;
+    system("cls");
+    while (true)
+    {
+        pole[cur] = true;
+    
+    
+        
+        
+        cout << pole[0] << " | "<< pole[1] << " | "<< pole[2] << endl;
+        cout << pole[3] << " | "<< pole[4] << " | "<< pole[5] << endl;
+        cout << pole[6] << " | "<< pole[7] << " | "<< pole[8] << endl;
+        pole[cur] = false;
+        if (cur == 8)
+        {
+            cur = 0;            /* code */
+        }
+        else{
+            cur++;
+        }
+        
+    
+    Sleep(400);
+    system("cls");
+    }
+}
